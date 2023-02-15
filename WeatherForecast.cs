5f769a1624ed2ae1dacd85e9bda2e7c24460a693
huynamboz <teahuynam2003@gmail.com>
2023-02-15 18:05:03 +0700

    new base

---
+++ WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace e_commerce_server
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
