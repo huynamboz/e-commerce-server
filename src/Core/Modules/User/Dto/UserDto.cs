842ba7f5fc6ea0c8d1d6073f892a1867e0d08c0a
Nguyen Viet Nhat Long <95393817+RedHoes@users.noreply.github.com>
2023-04-08 22:32:16 +0700

    feat: Long - view user's profile (#18)
    Co-authored-by: RedHoes <longpd03@gmail.com>

---
+++ src/Core/Modules/User/Dto/UserDto.cs
@@ -0,0 +1,29 @@
+﻿using e_commerce_server.src.Core.Database.Data;
+using System.ComponentModel.DataAnnotations;
+
+namespace e_commerce_server.src.Core.Modules.User.Dto
+{
+    public class UserDto
+    {
+        public int id { get; set; }
+        [StringLength(250)]
+        public string email { get; set; }
+        [MaxLength(250)]
+        public string password { get; set; }
+        [MaxLength(250)]
+        public string name { get; set; }
+        public int role_id { get; set; }
+        [StringLength(20)]
+        public string? phone_number { get; set; }
+        public string? address { get; set; }
+        public bool? gender { get; set; }
+        public DateTime? birthday { get; set; }
+        public string? avatar { get; set; }
+        public string? reset_token { get; set; }
+        public DateTime? reset_token_expiration_date { get; set; }
+        public DateTime created_at { get; set; }
+        public int? district_id { get; set; }
+        public string? refresh_token { get; set; }
+        public virtual ICollection<ProductData> products { get; set; }
+    }
+}
