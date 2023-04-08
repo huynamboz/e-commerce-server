df4c9186e03700e472b493ae6febaa42bd16b3bc
RedHoes <longpd03@gmail.com>
2023-04-08 16:29:18 +0700

    feat: Long - edit user information

--- src/Core/Modules/Auth/Service/AuthEnum.cs
+++ src/Core/Modules/Auth/Service/AuthEnum.cs
@@ -5,6 +5,7 @@
         public const string LOGIN_SUCCESS = "Login successfully";
         public const string LOGIN_INCORRECT = "Email or password is incorrect";
         public const string DUPLICATE_EMAIL = "This email is already exists";
+        public const string DUPLICATE_PHONE_NUMBER = "This phone number is already exists";
         public const string REGISTER_SUCCESS = "Register successfully";
         public const string INVALID_REFRESH_TOKEN = "Invalid refresh token";
     }
--- src/Core/Modules/User/Service/UserService.cs
+++ src/Core/Modules/User/Service/UserService.cs
@@ -1,9 +1,20 @@
 using e_commerce_server.Src.Core.Database.Data;
+using e_commerce_server.Src.Packages.HttpException;
+using e_commerce_server.src.Core.Modules.User.Dto;
+using e_commerce_server.Src.Core.Modules.Auth.Service;
 
 namespace e_commerce_server.Src.Core.Modules.User.Service
 {
     public class UserService
     {
+        private UserRepository userRepository;
+        private BCryptService bCryptService;
+        public UserService(MyDbContext context)
+        {
+            userRepository = new UserRepository(context);
+            bCryptService = new BCryptService();
+        } 
+    
         public bool CheckUserStatus(UserData user)
         {
             return !String.IsNullOrEmpty(user.email) &&
@@ -15,6 +26,43 @@ namespace e_commerce_server.Src.Core.Modules.User.Service
                 user.birthday != null &&
                 !String.IsNullOrEmpty(user.avatar) &&
                 Convert.ToBoolean(user.district_id);
+        }
+
+        public object UpdateUserById(UpdateUserDto updateUserDto, int userId)
+        {
+            var userByEmail = userRepository.GetUserByEmail(updateUserDto.email);
+            
+            if (userByEmail != null && userByEmail.id != userId)
+            {
+                throw new BadRequestException(AuthEnum.DUPLICATE_EMAIL);
+            }
+
+            if (!String.IsNullOrEmpty(updateUserDto.phone_number))
+            {
+                var userByPhone = userRepository.GetUserByPhoneNumber(updateUserDto.phone_number);
+
+                if (userByPhone != null && userByPhone.id != userId)
+                {
+                    throw new BadRequestException(AuthEnum.DUPLICATE_PHONE_NUMBER);
+                }
+            }
+
+            var userById = userRepository.GetUserById(userId) ?? throw new BadRequestException(UserEnum.USER_NOT_FOUND);
+            userById.name = updateUserDto.name;
+            userById.email = updateUserDto.email;
+            userById.phone_number = updateUserDto.phone_number;
+            userById.password = bCryptService.Hash(updateUserDto.password);
+            userById.gender = updateUserDto.gender;
+            userById.birthday = updateUserDto.birthday;
+            userById.address = updateUserDto.address;
+            userById.avatar = updateUserDto.avatar;
+            userById.district_id = updateUserDto.district_id;
+
+            return new
+            {
+                message = UserEnum.UPDATE_USER_SUCCESS,
+                data = userRepository.UpdateUser(userById)
+            };
         }
     }
 }
