6bdc4e0c2dd776d98f393f944fb5318261604006
Nguyen Viet Nhat Long <95393817+RedHoes@users.noreply.github.com>
2023-04-26 14:24:27 +0700

    feat: Long - create and update report (#41)
    * feat: Long - create or update report by id
    
    ---------

---
+++ src/Core/Modules/Report/ReportRepository.cs
@@ -0,0 +1,50 @@
+﻿using e_commerce_server.src.Core.Database;
+using e_commerce_server.src.Core.Database.Data;
+using e_commerce_server.src.Packages.HttpExceptions;
+
+namespace e_commerce_server.src.Core.Modules.Report
+{
+    public class ReportRepository
+    {
+        private readonly MyDbContext _context;
+        public ReportRepository(MyDbContext context)
+        {
+            _context = context;
+        }
+        public ReportData? GetReportByIds(int userId, int productId)
+        {
+            try
+            {
+                return _context.Reports.SingleOrDefault(r => r.user_id == userId && r.product_id == productId);
+            }
+            catch (Exception ex)
+            {
+                throw new InternalException(ex.Message);
+            }
+        }
+        public void CreateReport(ReportData report)
+        {
+            try
+            {
+                _context.Reports.Add(report);
+                _context.SaveChanges();
+            }
+
+            catch (Exception ex)
+            {
+                throw new InternalException(ex.Message);
+            }
+        }
+        public void UpdateReport(ReportData report)
+        {
+            try
+            {
+                _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw new InternalException(ex.Message);
+            }
+        }
+    }
+}
---
+++ src/Core/Modules/Report/Service/ReportEnum.cs
@@ -0,0 +1,9 @@
+﻿namespace e_commerce_server.src.Core.Modules.Report.Service
+{
+    public class ReportEnum
+    {
+        public const string UPDATE_REPORT_SUCCESS = "Update report successfully";
+        public const string CREATE_REPORT_SUCCESS = "Create report successfully";
+        public const string CANNOT_REPORT_OWN_PRODUCT = "You cannot report your own product";
+    }
+}
